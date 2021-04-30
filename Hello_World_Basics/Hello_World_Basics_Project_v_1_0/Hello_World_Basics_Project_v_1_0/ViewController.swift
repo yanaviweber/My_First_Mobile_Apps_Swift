b5550792512027b2986f7f7b78b348c6322920f2
Vi Jane <creative.mashine@gmail.com>
2021-04-30 21:57:07 +0300

    Basics intro in Xcode - theory and practice

---
+++ Hello_World_Basics/Hello_World_Basics_Project_v_1_0/Hello_World_Basics_Project_v_1_0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello_World_Basics_Project_v_1_0
+//
+//  Created by Viktoriia Jane Ro on 30.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
