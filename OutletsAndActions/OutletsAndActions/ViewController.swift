53715f3d5165ddc81c826e716b0e218f1c7cb1de
Vi Jane <creative.mashine@gmail.com>
2021-07-30 23:03:53 +0300

    start: Begin work of project. Create project OutletsAndActions.

---
+++ OutletsAndActions/OutletsAndActions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OutletsAndActions
+//
+//  Created by Viktoriia Jane Ro on 30.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
