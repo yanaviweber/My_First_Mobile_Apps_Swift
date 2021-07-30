c9420e5b82e555488f2676f324a86241bfcf5a00
Vi Jane <creative.mashine@gmail.com>
2021-07-30 23:33:00 +0300

    feat: finish lesson "IBOutlets"

--- OutletsAndActions/OutletsAndActions/ViewController.swift
+++ OutletsAndActions/OutletsAndActions/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    
+    @IBOutlet var labelCollection: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+ 
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        print("Hello World")
+        label.text = "Hello Label"
+    }
+    
 }
 
