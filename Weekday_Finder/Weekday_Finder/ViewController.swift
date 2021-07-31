eeb9ffd7714e0c75bbe94d99d16aa5c206db45fe
Vi Jane <creative.mashine@gmail.com>
2021-08-01 00:28:03 +0300

    start: begin create an app Weekday Finder

---
+++ Weekday_Finder/Weekday_Finder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weekday_Finder
+//
+//  Created by Viktoriia Jane Ro on 01.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
