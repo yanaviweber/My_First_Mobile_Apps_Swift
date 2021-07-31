063a194ad242b7379d86978f617ec513dc331216
Vi Jane <creative.mashine@gmail.com>
2021-08-01 02:13:53 +0300

    fix: Edit date format with guard instructions and delete optionals in code

--- Weekday_Finder/Weekday_Finder/ViewController.swift
+++ Weekday_Finder/Weekday_Finder/ViewController.swift
@@ -18,18 +18,20 @@ class ViewController: UIViewController {
     
     @IBAction func findDay(_ sender: UIButton) {
         
+        
+        guard let day = dateTF.text, let month = monthTF.text, let year = yearTF.text else { return }
+        
         let calendar = Calendar.current
         var dateComponents = DateComponents()
-        dateComponents.day = Int(dateTF.text!)
-        dateComponents.month = Int(monthTF.text!)
-        dateComponents.year = Int(yearTF.text!)
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
         
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "EEEE"
         
-        
-        let date = calendar.date(from: dateComponents)
-        let weekday = dateFormatter.string(from: date!)
+        guard let date = calendar.date(from: dateComponents) else { return }
+        let weekday = dateFormatter.string(from: date)
         
         resultLabel.text = weekday
     }
