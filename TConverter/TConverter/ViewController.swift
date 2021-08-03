b3ff305aa97d4500282743ef678196feb5d9667f
Vi Jane <creative.mashine@gmail.com>
2021-08-03 20:16:05 +0300

    feat: set parameters in code in the ViewController for slider. test app

--- TConverter/TConverter/ViewController.swift
+++ TConverter/TConverter/ViewController.swift
@@ -12,7 +12,13 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var cenliusLabel: UILabel!
     @IBOutlet weak var fahrenheitLabel: UILabel!
-    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.maximumValue = 100
+            slider.minimumValue = 0
+            slider.value = 0
+        }
+    }
     
     @IBAction func sliderChanged(_ sender: UISlider) {
     }
