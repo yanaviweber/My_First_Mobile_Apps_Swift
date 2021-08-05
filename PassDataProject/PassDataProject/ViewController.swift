c6d34a43f3c9da1750c2c7567f146b3d09d41c5d
Vi Jane <creative.mashine@gmail.com>
2021-08-06 02:54:09 +0300

    feat: set connections with interface and code fo text fields and button in the ViewController

--- PassDataProject/PassDataProject/ViewController.swift
+++ PassDataProject/PassDataProject/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var loginTF: UITextField!
+    
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
     }
-
-
+    
 }
 
