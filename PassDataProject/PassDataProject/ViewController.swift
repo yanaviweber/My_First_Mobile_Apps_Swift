e2ae7c9536604f6bd8f0f65ebb295de167cd34ed
Vi Jane <creative.mashine@gmail.com>
2021-08-06 02:18:48 +0300

    start: create the app PassDataProject

---
+++ PassDataProject/PassDataProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassDataProject
+//
+//  Created by Viktoriia Jane Ro on 06.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
