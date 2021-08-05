045315c306ad8d5baffe019569c080fb51e95f81
Vi Jane <creative.mashine@gmail.com>
2021-08-06 02:47:28 +0300

    feat: create new file for new second controller - SecondViewController.swift

---
+++ PassDataProject/PassDataProject/SecondViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.swift
+//  PassDataProject
+//
+//  Created by Viktoriia Jane Ro on 06.08.2021.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+
+}
