570bd82788d449d39fb2d8084684220eb6a965eb
Vi Jane <creative.mashine@gmail.com>
2021-08-06 02:58:19 +0300

    feat: set connections with interface and code for title and button to the SecondViewController

--- PassDataProject/PassDataProject/SecondViewController.swift
+++ PassDataProject/PassDataProject/SecondViewController.swift
@@ -9,5 +9,11 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
-
+    @IBOutlet weak var label: UILabel!
+    
+    
+    
+    @IBAction func goBackTapped(_ sender: UIButton) {
+    }
+    
 }
