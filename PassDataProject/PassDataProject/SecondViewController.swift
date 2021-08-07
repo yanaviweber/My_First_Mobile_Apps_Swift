5b09cb3473d6809a1f095bd1de5b952a57127997
Vi Jane <creative.mashine@gmail.com>
2021-08-07 03:18:35 +0300

    fix: set parameter name without "Optional" in the SecondViewController for output params for the device display

--- PassDataProject/PassDataProject/SecondViewController.swift
+++ PassDataProject/PassDataProject/SecondViewController.swift
@@ -14,6 +14,7 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        guard let login = self.login else {return}
         label.text = "Hello, \(login)"
     }
     
