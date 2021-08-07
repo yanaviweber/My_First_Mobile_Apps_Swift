e34a1efa952de8f7c8267f9b2d4e6c7d3f30e0ec
Vi Jane <creative.mashine@gmail.com>
2021-08-07 04:02:42 +0300

    feat: add new functionality for get data on the SecondViewController to the ViewController by unwind segue

--- PassDataProject/PassDataProject/ViewController.swift
+++ PassDataProject/PassDataProject/ViewController.swift
@@ -13,13 +13,16 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var passwordTF: UITextField!
     
+    @IBOutlet weak var resultLabel: UILabel!
+    
     
     @IBAction func loginTapped(_ sender: UIButton) {
         performSegue(withIdentifier: "detailSegue", sender: nil)
     }
     
     @IBAction func unwindSegueToMainScreen(segue: UIStoryboardSegue){
-        
+        guard let svc = segue.source as? SecondViewController else {return}
+        self.resultLabel.text = svc.label.text
     }
     
     
